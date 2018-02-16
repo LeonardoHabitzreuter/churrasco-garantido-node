7d82190d7e223f0ef0d759cb2a7d03614ac83660
leonardohabitzreuter <leo.habitzreuter@gmail.com>
2018-02-16 21:32:51 -0200

    corrigido teste extremamente importante da criacao de usuario

--- backend/tests/api/companies/companiesService.tests.js
+++ backend/tests/api/companies/companiesService.tests.js
@@ -9,25 +9,36 @@ const server = require('../../../src/infraestructure/server')
 require('../../../src/infraestructure/router')(server)
 const Company = require('../../../src/api/companies/company')
 
-describe('companiesRequests', () => {
-    let createStub = sinon.stub(Company, 'create')
-    let findOneStub = sinon.stub(Company, 'findOne').callsFake((query, callback) => { callback(null, { name: 'teste ind', cnpj: '82746377000159' }) })
+const Test_Token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVhODRhZDlmNjBkNDk3MjNjNzhlNDM0YSIsImlhdCI6MTUxODgxODQ2MiwiZXhwIjoxNTE4ODQwMDYyfQ.yUkOPKBDTIaVxTUNlzycJrhRlRBbuco8YKkOxqhpghQ'
 
-    it('should return an error trying to create a company when some company already registered has the same cnpj', () => {
+describe.only('companiesRequests', () => {
+    let createStub
+    let findOneStub
+
+    beforeEach(() => {
+        createStub = sinon.stub(Company, 'create')
+        findOneStub = sinon.stub(Company, 'findOne').callsFake((query, callback) => { callback(null, { name: 'teste ind', cnpj: '82746377000159' }) })
+    })
+
+    afterEach(() => {
+        createStub.restore()
+        findOneStub.restore()
+    })
+
+    it('should return an error trying to create a company when some company already registered has the same cnpj', done => {
         request(server)
-            .post('/companies')
+            .post('/api/companies')
+            .set('Authorization', Test_Token)
             .send({
                 name: 'Leonardo Habitzreuter',
                 cnpj: '82.746.377/0001-59'
             })
-            .expect('Content-Type', /json/)
             .expect(400)
             .end((err, res) => {
-                if (err) throw err
+                expect(err).to.be.null
+                expect(createStub).to.not.have.been.called
+                done()
+                expect(res.text).to.eql('{"errors":["O CNPJ já foi cadastrado para outra empresa."]}')
             })
-
-        expect(createStub).to.not.have.been.called
-        createStub.restore()
-        findOneStub.restore()
     })
 })
