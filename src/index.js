c4f562b095d7bd2d9bab7d3f4a728be240799989
leonardohabitzreuter <leo.habitzreuter@gmail.com>
2018-03-10 20:34:22 -0300

    configurado clusters de acordo com o número de núcleos do processadore

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-const server = require('./infraestructure/server')
+const cluster = require('cluster')
+const CPUsAmount = require('os').cpus().length
 require('./infraestructure/database')
-require('./infraestructure/router')(server)
+
+if (cluster.isMaster) {
+  for (let i = 0; i < CPUsAmount; i++) {
+    cluster.fork()
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`)
+  })
+} else {
+  const server = require('./infraestructure/server')
+  require('./infraestructure/router')(server)
+}
