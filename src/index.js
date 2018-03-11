4b55bf3575d95f094eabd6d1ab9bd28c2772c6e6
leonardohabitzreuter <leo.habitzreuter@gmail.com>
2018-03-11 15:43:54 -0300

    alterada criacao dos clusters para default do pm2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
-const cluster = require('cluster')
-const CPUsAmount = require('os').cpus().length
 require('./infraestructure/database')
-
-if (cluster.isMaster) {
-  for (let i = 0; i < CPUsAmount; i++) {
-    cluster.fork()
-  }
-
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`)
-    cluster.fork()
-  })
-} else {
-  const server = require('./infraestructure/server')
-  require('./infraestructure/router')(server)
-}
+const server = require('./infraestructure/server')
+require('./infraestructure/router')(server)
