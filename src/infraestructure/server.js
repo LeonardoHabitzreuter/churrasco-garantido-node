5a4c20dafcaae370a1955fff357da8f3f3d2acc4
leonardo.h <leonardo.h@hbsis.com.br>
2018-06-13 07:58:34 -0300

    mudado servidor http para https do express

--- src/infraestructure/server.js
+++ src/infraestructure/server.js
@@ -13,6 +13,16 @@ server.use(bodyParser.json())
 server.use(allowCors)
 server.use(queryParser())
 
-server.listen(port, '0.0.0.0', () => console.log('server running on port 3001'))
+// https://aghassi.github.io/ssl-using-express-4/
+var fs = require('fs')
+const credentials = {
+  key: fs.readFileSync('sslcert/key.pem'),
+  cert: fs.readFileSync('sslcert/cert.pem')
+}
+const https = require('https')
+https
+  .createServer(credentials, server)
+  .listen(port, '0.0.0.0', () => console.log('server running on port 3001'))
+// server.listen(port, '0.0.0.0', () => console.log('server running on port 3001'))
 
 module.exports = server
